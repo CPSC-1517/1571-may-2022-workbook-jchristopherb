0293caa3031b781647c3e6d6138cb8e58dbab764
ChrisBana <jbana1@nait.ca>
2022-06-20 17:09:13 -0600

    update

--- CSRazerSolution/WebApp/Pages/Index.cshtml.cs
+++ CSRazerSolution/WebApp/Pages/Index.cshtml.cs
@@ -3,30 +3,48 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
 {
+    //this web page Model class inherits from PageModel
     public class IndexModel : PageModel
     {
+        //this default page uses a system class called ILogger<T>
+        //this is composition
+        //this is a local field
         private readonly ILogger<IndexModel> _logger;
 
+        //constructor
+        //this constructor receive an injection of a service
+        //this injection is referred to as Injection Dependency
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+        //this is a local property
         public string MyName { get; set; }
 
+        //this is a class Behaviour (method)
+        //this method, OnGet(), executes for any Get request
+        //this method will be the first method executed when the page is first
+        //  used.
+        //excellent "event" to use to do any initialization to your web page display
+        
         public void OnGet()
         {
+            //once in the request method, you are in control of what is being
+            //  processed on the web page for the current request
+
             Random rnd = new Random();
-            int value = rnd.Next(0,100);    //  100 is NOT included
+            int value = rnd.Next(0,100); //100 is NOT include
             if (value % 2 == 0)
             {
-                MyName = $"Chris ({value}) welcome to your Razon Pages";
+                MyName = $"Don ({value}) welcome to the wide wild world of Razor Pages";
             }
             else
             {
                 MyName = null;
             }
 
+            //control is returned to the web server
         }
     }
 }
---
+++ CSRazerSolution/WebApp/ReadMe.md
@@ -0,0 +1,7 @@
+# 1517 May 2022 workbook
+
+## John Christopher Bana
+
+## jchristopherb
+
+This is a public repository for your classroom demonstrations and practice. You will use this only for public work. Exercise work submitted within this repository will NOT be evaluated.
