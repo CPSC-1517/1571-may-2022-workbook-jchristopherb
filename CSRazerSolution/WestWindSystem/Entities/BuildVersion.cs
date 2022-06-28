e449203b40b413159d10c8c90804f266b61f6b6b
ChrisBana <jbana1@nait.ca>
2022-06-27 18:56:34 -0600

    adding classes DB

---
+++ CSRazerSolution/WestWindSystem/Entities/BuildVersion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.Entities
+{
+    public class BuildVersion
+    {
+        //  in sql nvarchar(), varchar, nchar, char is declared as a STRING in your ENTITY definition
+        //  sql BIT IS A BOOL IN c#
+        public int Id { get; set; }
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
